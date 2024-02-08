325c417a9b9d60a5b6014f691df7664a5cf6eb61
Romain <98345704+Romlion@users.noreply.github.com>
2024-02-08 17:18:56 +0100

    file changed

--- src/components/Formulaire/ContactForm.jsx
+++ src/components/Formulaire/ContactForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import styles from "./ContactForm.module.css"
+import styles from "./ContactForm.module.scss"
 import NavBar from "../NavBar/NavBar";
 
 
